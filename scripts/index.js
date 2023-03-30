fc8289233383555bba900368812886b1c86b4f15
Alexandr Gerasimov <sashagerasimov55@gmail.com>
2023-03-30 18:28:58 +0300

    Проектная работа 5

--- scripts/index.js
+++ scripts/index.js
@@ -1,38 +1,131 @@
-let popup = document.querySelector('.popup');
-let addButton = document.querySelector('.button_action_edit');
-let closeButton = document.querySelector('.button_action_close');
-// =========================================
-let nameProfile = document.querySelector('.profile__name');
-let jobProfile = document.querySelector('.profile__job');
-let nameInput = document.querySelector('.popup__text_type_name');
-let jobInput = document.querySelector('.popup__text_type_job');
-// =========================================
-let formElement = document.querySelector('.popup__container');
+// Кнопки
+const buttonEditProfile = document.querySelector('.profile__button-edit');
+//const closeButton = document.querySelector('.popup__button-close');
+const buttonAddNewCard = document.querySelector('.profile__button-add');
+//const submitButton = document.querySelector('.button_action_submit');
+const buttonDeleteCard = document.querySelector('.element__button-delete');
 
+// Переменные из секции Профиль
+const nameProfile = document.querySelector('.profile__name');
+const jobProfile = document.querySelector('.profile__job');
+
+// Попап
+const popup = document.querySelector('.popup');
+
+// Попап Редактирования профиля
+const popupTypeEdit = document.querySelector('.popup_type_edit');
+const buttonCloseProfile = popupTypeEdit.querySelector('.popup__button-close');
+const nameInput = popupTypeEdit.querySelector('.popup__text_type_name');
+const jobInput = popupTypeEdit.querySelector('.popup__text_type_job');
+const formElementProfile = popupTypeEdit.querySelector('.popup__container');
+
+// Попап Добавления Новых Карточек
+const popupTypeNewCard = document.querySelector('.popup_type_new-card');
+const buttonCloseCard = popupTypeNewCard.querySelector('.popup__button-close');
+const titleImg = popupTypeNewCard.querySelector('.popup__text_type_title');
+const srcImg = popupTypeNewCard.querySelector('.popup__text_type_src');
+const formElementImg = popupTypeNewCard.querySelector('.popup__container');
+
+// Попап Просмотра Картинок
+const popupViewImg = document.querySelector('.popup_type_view-img');
+const buttonCloseImg = popupViewImg.querySelector('.popup__button-close');
+const popupTitleImg = popupViewImg.querySelector('.popup__caption');
+const popupScalableImg = popupViewImg.querySelector('.popup__img');
+
+//======================
+const cardList = document.querySelector('.elements__items');
+const cardTemplate = document.querySelector('#cards-element').content;
+
+// Функция открытия Попапа
+function openPopup(popupElement) {
+	popupElement.classList.add('popup_opened');
+}
+
+// Функция закрытия Попапа
+function closePopup(popupElement) {
+	popupElement.classList.remove('popup_opened');
+}
+
+// Функция которая при открытии заполняет Форму данными с Профиля
 function handleEditProfileClick() {
+	openPopup(popupTypeEdit);
 	nameInput.value = nameProfile.textContent;
 	jobInput.value = jobProfile.textContent;
 }
 
-function openPopup() {
-	popup.classList.add('popup_opened');
-	handleEditProfileClick();
-}
+// Обработчик события ОТКРЫТИЯ Попапа Редактирования Профиля
+buttonEditProfile.addEventListener('click', handleEditProfileClick)
 
-function closePopup() {
-	popup.classList.remove('popup_opened');
-}
+// Обработчик события ОТКРЫТИЯ Попапа Добавления Карточек
+buttonAddNewCard.addEventListener('click', () => {
+	openPopup(popupTypeNewCard);
+});
 
-addButton.addEventListener('click', openPopup)
-closeButton.addEventListener('click', closePopup);
+// Обработчик события ЗАКРЫТИЯ Попапа Редактирования Профиля
+buttonCloseProfile.addEventListener('click', function () {
+	closePopup(popupTypeEdit);
+});
 
-function handleFormSubmit(evt) {
+// Обработчик события ЗАКРЫТИЯ Попапа Добавления Карточек
+buttonCloseCard.addEventListener('click', function () {
+	closePopup(popupTypeNewCard);
+});
+
+// Обработчик события ЗАКРЫТИЯ Попапа Просмотра Картинки
+buttonCloseImg.addEventListener('click', function () {
+	closePopup(popupViewImg);
+});
+
+// Функция сохраняющая введенные пользователем данные в Профиль
+function handleProfileFormSubmit(evt) {
 	evt.preventDefault();
-
 	nameProfile.textContent = nameInput.value;
 	jobProfile.textContent = jobInput.value;
-
-	closePopup();
+	closePopup(popupTypeEdit);
 }
 
-formElement.addEventListener('submit', handleFormSubmit);
+// Обработчик события ОТПРАВКИ формы Редактирования Профиля
+formElementProfile.addEventListener('submit', handleProfileFormSubmit);
+
+//Функция добавления карточки через попап
+function addCard(srcImg, titleImg) {
+	const cardElement = cardTemplate.querySelector('.element').cloneNode(true);
+
+	cardElement.querySelector('.element__image').src = srcImg;
+	cardElement.querySelector('.element__image').alt = titleImg;
+	cardElement.querySelector('.element__title').textContent = titleImg;
+	cardElement.querySelector('.element__button-like').addEventListener('click', function (evt) {
+		evt.target.classList.toggle('element__button-like_active');
+	});
+	cardElement.querySelector('.element__button-delete').addEventListener('click', function (evt) {
+		evt.target.closest('.element').remove();
+	})
+	cardElement.querySelector('.element__image').addEventListener('click', function () {
+		openPopup(popupViewImg);
+		popupTitleImg.textContent = titleImg;
+		popupScalableImg.src = srcImg;
+		popupScalableImg.alt = titleImg;
+	})
+
+	cardList.prepend(cardElement);
+	return cardElement;
+}
+
+//Функция перебора массива и вывод карточек на страницу при загрузке
+initialCards.forEach(function (element) {
+	const newCard = addCard(element.link, element.name);
+	cardList.append(newCard);
+});
+
+//Функция присваивания значений из попапа в Карточку
+function handleImgFormSubmit(evt) {
+	evt.preventDefault();
+	addCard(srcImg.value, titleImg.value);
+	titleImg.value = '';
+	srcImg.value = '';
+	closePopup(popupTypeNewCard);
+}
+
+// Обработчик события ОТПРАВКИ формы Добавления Карточки
+formElementImg.addEventListener('submit', handleImgFormSubmit);
+
